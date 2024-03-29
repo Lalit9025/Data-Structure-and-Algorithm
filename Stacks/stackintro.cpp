c9fbe4cf717a52980b59163340e200856ce8f4a2
Lalit9025 <lalit3chouhan@gmail.com>
2024-03-29 11:46:56 +0530

    stack intro

--- Stacks/stackintro.cpp
+++ Stacks/stackintro.cpp
@@ -2,12 +2,30 @@
 using namespace std;
 
 int main() {
-
+    //LIFO - last in first out
     //creation of stack
     stack<int> s;
 
     //push operation
     s.push(5);
     s.push(4);
-    
+
+    //pop operation
+    s.pop();
+
+    //top element
+    cout<<"top element : "<<s.top()<<endl;;
+
+    //checking stack is empty or not
+    if(s.empty()){
+        cout<<"stack is empty"<<endl;
+    } else{
+        cout<<"stack is not empty"<<endl;
+    }
+
+    //checking the size
+    cout<<"size of array : "<<s.size()<<endl;
+
+
+    return 0;
 }
